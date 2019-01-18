4fee0ae35f9f60165389bfc61aae3a59de4d43ed
samliu <sam.liu@avid.ly>
2019-01-18 11:27:14 +0800

    add some http request code

--- sdk/src/main/java/com/avidly/sdk/account/activity/BaseEditorActivity.java
+++ sdk/src/main/java/com/avidly/sdk/account/activity/BaseEditorActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -27,31 +28,31 @@ public abstract class BaseEditorActivity extends AppCompatActivity {
     }
 
     private void hideBottomUI() {
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-            View v = this.getWindow().getDecorView();
-            v.setSystemUiVisibility(View.GONE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
-            decorView.setSystemUiVisibility(uiOptions);
-        }
+//        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+//            View v = this.getWindow().getDecorView();
+//            v.setSystemUiVisibility(View.GONE);
+//        } else if (Build.VERSION.SDK_INT >= 19) {
+//            View decorView = getWindow().getDecorView();
+//            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+//            decorView.setSystemUiVisibility(uiOptions);
+//        }
     }
 
     private void setSystemUIVisible(boolean show) {
-        if (show) {
-            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            uiFlags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            getWindow().getDecorView().setSystemUiVisibility(uiFlags);
-        } else {
-            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
-            uiFlags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            getWindow().getDecorView().setSystemUiVisibility(uiFlags);
-        }
+//        if (show) {
+//            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//            uiFlags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//            getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+//        } else {
+//            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+//            uiFlags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//            getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+//        }
     }
 
     @Override
@@ -66,6 +67,7 @@ public abstract class BaseEditorActivity extends AppCompatActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
+        Log.i("xxx", "onWindowFocusChanged hasfocus:" + hasFocus);
         if (hasFocus && Build.VERSION.SDK_INT >= 19) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
---
+++ sdk/src/main/java/com/avidly/sdk/account/request/HttpRequest.java
@@ -0,0 +1,56 @@
+package com.avidly.sdk.account.request;
+
+import com.avidly.sdk.account.base.http.HttpClientHelper;
+import com.avidly.sdk.account.base.http.HttpResponse;
+import com.avidly.sdk.account.base.http.UrlQuery;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpRequest {
+    private static ExecutorService mSingleThreadPool = Executors.newSingleThreadExecutor();
+
+    public static void requestHttpByPost(final String url, final Map<String, String> params, final HttpCallback<String> callback) {
+        mSingleThreadPool.execute(new RunWrapper(new Runnable() {
+            @Override
+            public void run() {
+                int code = 0;
+                String result = null;
+                String request = new UrlQuery().addParams(params).toString();
+                try {
+                    HttpResponse response = HttpClientHelper.httpPost(url, request, null);
+                    code = response.getResponseCode();
+                    if (code == 200) {
+                        result = response.getBody();
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    if (null != callback) {
+                        callback.onResponedFail(e, code);
+                    }
+                    return;
+                }
+
+                if (null != callback) {
+                    callback.onResponseSuccess(result);
+                }
+            }
+        }));
+    }
+
+    private static class RunWrapper implements Runnable {
+        Runnable runnable;
+
+        RunWrapper(Runnable r) {
+            runnable = r;
+        }
+
+        public void run() {
+            try {
+                runnable.run();
+            } catch (Throwable e) {
+            }
+        }
+    }
+}
